dfbc2d51e4b8bd101f6800346e58f0b825e06622
Andi R. Hermawan <andi11@mhs.ee.its.ac.id>
2020-10-15 15:34:35 +0700

    fix: add viewport

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,9 @@ export default function Home() {
     <div>
       <Head>
         <title>Build Your Own Shoes</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <MainPage />
     </div>
   );
