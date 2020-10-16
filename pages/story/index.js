6eb41f33610988cbe2239305d330932268443aff
Andi R. Hermawan <andirkh@users.noreply.github.com>
2020-10-17 05:33:53 +0700

    fix: page name

--- pages/story/index.js
+++ pages/story/index.js
@@ -31,6 +31,7 @@ export default function Home() {
         <h1 className="mb-5">Hello There,</h1>
         <p className="text-monospace">{`I'm Andi (@andirkh), on the last 2 years I'm working for the one of Texas - Austin based company as a Remote UI Software Engineer.`}</p>
         <p className="text-monospace">{`This project is fun project to keep myself always creative`}</p>
+        <p className="text-monospace">{`I do have several fixes or features. Here's the list of them :`}</p>
       </div>
     </div>
   );
